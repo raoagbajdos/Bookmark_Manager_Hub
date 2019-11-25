6ca0246c5271400fc71e04a65db06ea33d95322f
Richard <aagbajdos@gmail.com>
2019-11-25 17:41:17 +0000

    setting up a database

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Bookmark Manager'
-  end
+  get '/bookmarks' do
+      @bookmarks = Bookmark.all
+      erb :'bookmarks/index'
+    end
 
   get '/bookmarks' do
     @bookmarks = [
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,6 +1,5 @@
-require './bookmark'
 
-describe Bookmark do
+describe 'Bookmark' do
   describe '.all' do
     it 'returns all bookmarks' do
       bookmarks = Bookmark.all
